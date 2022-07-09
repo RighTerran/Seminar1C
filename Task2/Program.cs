da3738c900e37fbff8851414e08c591e1ef6b218
Dmitriy <sctoss@gmail.com>
2022-07-09 12:05:18 +0300

    Changed Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if(a > b)
+{
+    Console.WriteLine("Максимальное число = " + a);
+}
+else
+{
+    Console.WriteLine("Максимальное число = " + b);
+}
